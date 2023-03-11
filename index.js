039cf29ae5bf4896c572ea40a2d9367eed732b77
virajaher <virajaher7@gmail.com>
2023-03-11 13:34:02 +0530

    CSS complete 2

---
+++ index.js
@@ -0,0 +1,5 @@
+// add function that triggers the onclick event of the points buttons and adds points according to the buttons.
+// reset all the values to zero with the onclick event of the restart button
+// add fouls by using subtract funtion and creating button for foul
+// timer can be place around restart button and we can use laps that are similar to the passenger counter app
+// maxpoints = 99
